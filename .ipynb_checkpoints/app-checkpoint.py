ea65e64b58085fddc877607f85c8e4c7f1843929
Surajpatel28 <suraj123patel123@gmail.com>
2025-03-07 17:35:29 +0530

    created backend and working on the fronend

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+import pandas as pd
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) 
+
+# loading the model
+model = joblib.load("linear_regression_car_price")
+
+@app.route("/")
+def home():
+    return "car price Prediction API is running !"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    try:
+        user_input = np.array([[
+            int(data["year"][0]),
+            float(data["Present_Price"][0]),
+            int(data["Kms_Driven"][0]),
+            int(data["Fuel_Type"][0]),
+            int(data["Seller_Type"][0]),
+            int(data["Transmission"][0]),
+            int(data["Owner"][0])
+        ]]   
+        )
+    except KeyError as e:
+        return jsonify({"error": f"Missing Feature: {str(e)}"}), 400
+    except ValueError:
+        return jsonify({"error": "Invalid Input Type. Please Enter correct values."}), 400
+
+    prediction = model.predict(user_input)[0]
+
+    return jsonify({
+        "Car Price: ": f"₹{prediction}"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
